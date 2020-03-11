0d09b38943d6fff345ff95aeba898f6a8cc7b81f
iachievedream <iachievedream@gmail.com>
2020-03-11 15:28:52 +0800

    try another way in jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,23 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facede\Auth;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function __construct()
     {
-    	$this->middleware('auth:api')->except('login');
+    	$this->middleware('auth:api',['except' => ['login']]);
     }
     public function login()
     {
-    	$credentials = Request(['email','password']);
+    	$credentials = request(['email','password']);
 
     	if (! $token = auth()->attempt($credentials)) {
-    		return response()->json(['status' => 1, 'message' => 'invalid credentials'],401);
+    		return response()->json(['error' => 'Unauthorized'],401);
     	}
 
-    	return response()->json(['status' => 0, 'token' =>$token]);
+    	return $this->respondWithToken($token);
     }
 
     public function me()
@@ -28,6 +30,18 @@ class AuthController extends Controller
     public function logout()
     {
     	auth()->logout();
-    	return response()->json(['status' = 0]);
+    	return response()->json(['message' => 'Successfully logged out']);
+    }
+    public function refresh()
+    {
+    	return $this->respondWithToken(auth()->refresh());
+    }
+    public function respondWithToken($token)
+    {
+    	return response()->json([
+    		'access_token' => $token,
+    		'token_type' => 'bearer',
+    		'expies_in' => auth()->factory()->getTTL()*60
+    	]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -38,6 +38,7 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
     ];
     public function getJWTIdentifier()
+    
     {
         return $this->getKey();
     }
